215568306a963f057a0f2477e1eaafaf39debc94
Devesh Wadhwani <devesh.wadhwani123@gmail.com>
2022-12-10 17:48:15 +0530

    vercel setting

--- DiabetesPrediction/settings.py
+++ DiabetesPrediction/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'blq_5ogilm4io^a7&0vb#nbg*&jo719r$!l1)kyw72p@lk(&@8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
